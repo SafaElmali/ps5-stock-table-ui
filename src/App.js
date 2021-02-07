08127dce2d3f92b4a9a2686d04b30b266f09e804
SafaElmali <tsafaelmali@gmail.com>
2021-02-07 15:03:31 +0300

    refactor: modify spinner styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 import StockTable from "./components/StockTable";
 import Header from "./components/Header";
 import { useEffect, useState } from "react";
-import CircleLoader from "react-spinners/CircleLoader";
+import PacmanLoader from "react-spinners/PacmanLoader";
 import { ToastContainer, toast } from "react-toastify";
 import "./App.css";
 import moment from "moment";
-import 'moment/locale/tr';
+import "moment/locale/tr";
 
 function App() {
   const [stockData, setStockData] = useState([]);
@@ -34,7 +34,18 @@ function App() {
     }
   };
 
-  if (loading) return <CircleLoader size={100} />;
+  if (!loading)
+    return (
+      <Box
+        minHeight="100vh"
+        minWidth="100vw"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <PacmanLoader size={75} color={"#0080FF"} />
+      </Box>
+    );
 
   return (
     <Box className="sony-bg">
